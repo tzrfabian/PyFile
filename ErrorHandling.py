81e76c707c91bcd6da4a0ba8ef620fb0563b35fe
tzrfabian <fabiantenzara@gmail.com>
2024-03-12 22:47:11 +0700

    Adding ErrorHandling material

---
+++ ErrorHandling.py
@@ -0,0 +1,27 @@
+# n=2
+# try:
+#     z = int(input("Insert number: "))
+#     print(n/z)
+# except ZeroDivisionError:
+#     print("Cannot divide any number with Zero number.")
+    
+# var_dict = {"rata_rata": "2.5"}
+
+# try:
+#     print(f"rata-rata adalah {var_dict['rata_rata']}")
+# except KeyError:
+#     print("Key tidak ditemukan.")
+# except TypeError:
+#     print("Anda tidak bisa membagi nilai dengan tipe data string")
+# else:
+#     print("Kode ini dieksekusi jika tidak ada exception.")
+# finally:
+#     print("Kode ini dieksekusi terlepas dari ada atau tidaknya exception.")
+
+## Raise exception
+var = 10
+if var < 0:
+    raise ValueError("Negative number not allowed!")
+else:
+    for i in range (var):
+        print(i+1)
