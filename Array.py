a5bd4cc3418c52bc44cf91e011b293472c4b07ad
tzrfabian <fabiantenzara@gmail.com>
2024-03-14 22:37:55 +0700

    updating Array material

--- Array.py
+++ Array.py
@@ -19,15 +19,36 @@ print(type(x))
 
 # print(var_arr)
 
-var_arr = [1,2,3,4,5]
+# var_arr = [1,2,3,4,5]
 
-for i in range(len(var_arr)):
-    current_element = var_arr[i]
-    next_index = i+1
+# for i in range(len(var_arr)):
+#     current_element = var_arr[i]
+#     next_index = i+1
     
-    if next_index < len(var_arr):
-        next_element = var_arr[next_index]
-    else:
-        next_element = None
+#     if next_index < len(var_arr):
+#         next_element = var_arr[next_index]
+#     else:
+#         next_element = None
     
-    print(f"Current element: {current_element}, Next element: {next_element}")
+#     print(f"Current element: {current_element}, Next element: {next_element}")
+
+# var_arr = [1,7,2,89,3]
+# left_pointer = var_arr[0]
+
+# for i in range(1, len(var_arr)):
+#     right_pointer = var_arr[i]
+#     if right_pointer > left_pointer:
+#         left_pointer = right_pointer
+
+# print(left_pointer)
+
+var_array = [i for i in range(101)]
+total_element = 0
+count_element = 0
+for n in var_array:
+    total_element += n
+    count_element += 1
+
+result = total_element / count_element
+
+print(result)
